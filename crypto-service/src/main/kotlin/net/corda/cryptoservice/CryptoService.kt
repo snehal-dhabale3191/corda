77e2dec0d2e9d456ccba043dd4a1dfbf92fdd724
Kostas Chalkias <konstantinos.chalkias@r3.com>
2018-09-21 18:39:05 +0100

    create BCCryptoService to simulate an HSM

--- crypto-service/src/main/kotlin/net/corda/cryptoservice/CryptoService.kt
+++ crypto-service/src/main/kotlin/net/corda/cryptoservice/CryptoService.kt
@@ -5,7 +5,7 @@ import java.security.PublicKey
 interface CryptoService {
 
     /** schemeID is Corda specific. */
-    fun generateKeyPair(alias: String, schemeId: String): PublicKey
+    fun generateKeyPair(alias: String, schemeNumberID: String): PublicKey
     fun containsKey(alias: String): Boolean
     fun getPublicKey(alias: String): PublicKey
     fun sign(alias: String, data: ByteArray): ByteArray
